171004f65f6d8ffde3e7c095289c734845c8d98d
raahuldas <rahuldas42coc@gmail.com>
2024-07-27 17:06:45 +0530

    update

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -7,52 +7,59 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { MailIcon } from "lucide-react";
+import messages from "@/messages.json"
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-semibold mb-8 mt-12">
-        Welcome to NO-CAP Messages
-      </h1>
-      <h2 className="text-2xl ">
-        Send messages anonymously
-      </h2>
-      <Carousel className="w-full max-w-xs"
-        opts={{
-          align: "start",
-          loop: true
-        }}
-        plugins={[
-          Autoplay({
-            delay: 2000
-          })
-        ]}>
-        <CarouselContent className="p-10">
-          <CarouselItem>
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-semibold mb-8 mt-12">
+          Welcome to NO-CAP Messages
+        </h1>
+        <h2 className="text-2xl ">
+          Send messages anonymously
+        </h2>
+        <Carousel className="w-full max-w-xl"
+          opts={{
+            align: "start",
+            loop: true
+          }}
+          plugins={[
+            Autoplay({
+              delay: 2000
+            })
+          ]}>
+          <CarouselContent className="p-10">
 
-            <Card>
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
+            {
+              messages.map((message, index) => (
+                <CarouselItem key={index} >
+                  <Card className="bg-slate-100">
+                    <CardHeader>
+                      <CardTitle>{message.title}</CardTitle>
+                      <div className="flex items-center ">
+                        <div><MailIcon /></div>
+                        <div className="flex items-center">
+                          <CardContent className="py-0"> {message.content}
+                            <CardDescription>{message.recieved}</CardDescription>
+                          </CardContent>
+                        </div>
+                      </div>
+                    </CardHeader>
+                  </Card>
+                </CarouselItem>)
+              )}
 
-          </CarouselItem>
-          <CarouselItem><div className="text-center">Rahul</div></CarouselItem>
-          <CarouselItem><div className="text-center">das</div></CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-
-
-    </div>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+      <footer className="bg-slate-900 text-white text-center py-2 absolute bottom-0 w-full">
+        &copy; 2024 NO-CAP Messages. All rights reserved
+      </footer>
+    </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,22 +7,31 @@ import { User } from 'next-auth';
 import Link from 'next/link';
 
 function Navbar() {
-    const {data: session} = useSession();
-    
+    const { data: session } = useSession();
+
     const user: User = session?.user
     return (
         <div className='px-8 py-3 mx-auto shadow-md bg-slate-900 '>
             <div className="flex items-center justify-between">
                 <Link href="/" className="border py-2 px-3 rounded-sm bg-slate-300 cursor-pointer">
-                🧢 NO-CAP 
+                    🧢 NO-CAP
                 </Link>
                 <div className="uppercase text-white ">
-                     {user && "Welcome, " + (user.username || user.name)}
+                    {user && "Welcome, " + (user.username || user.name)}
                 </div>
                 <div>
-                    <Button className='bg-slate-700 hover:bg-red-600' onClick={() => signOut()}>
-                        LogOut
-                    </Button>
+                    {
+                        user ?
+                            <Button className='bg-slate-700 hover:bg-red-600' onClick={() => signOut()}>
+                                LogOut
+                            </Button>
+                            :
+                            <Link href="/signin">
+                            <Button className='bg-slate-700 hover:bg-slate-600'>
+                                Login
+                            </Button>
+                            </Link>
+                    }
                 </div>
             </div>
         </div>
